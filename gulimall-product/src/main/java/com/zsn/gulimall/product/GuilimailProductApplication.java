b6c0821a94231605cd8298dc9d2aa590b5caa336
zhousennan <zhousennan@ypzdw.com>
2024-06-21 15:48:45 +0800

    调整配置

--- gulimall-product/src/main/java/com/zsn/gulimall/product/GuilimailProductApplication.java
+++ gulimall-product/src/main/java/com/zsn/gulimall/product/GuilimailProductApplication.java
@@ -4,14 +4,29 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @MapperScan("com.zsn.gulimall.product.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GuilimailProductApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GuilimailProductApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(GuilimailProductApplication.class, args);
+
+        while(true) {
+            //当动态配置刷新时，会更新到 Enviroment中，因此这里每隔一秒中从Enviroment中获取配置
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+
+            //获取当前部署的环境
+            String currentEnv = applicationContext.getEnvironment().getProperty("current.env");
+           // System.err.println("in "+currentEnv+" enviroment; "+"user name :" + userName + "; age: " + userAge);
+            TimeUnit.SECONDS.sleep(1);
+        }
+
     }
 
 }
